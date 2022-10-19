57a7dd20aac2164cfb8c7ecc81b91bdd54ff24bb
alexisbolados <alexis.bolados@usach.cl>
2022-10-19 19:38:37 -0300

    default borrado, nuevo archivo home-screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build home screen scaffold
+    return const Scaffold(
+
+        //body: Text('Home screen')
+
+        );
+  }
+}
